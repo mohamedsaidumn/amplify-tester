14da5c6587aa102103bf4eb94500020dee2b7898
mohamedsaid321 <mohamedsaidumn@gmail.com>
2024-02-08 23:43:55 -0600

    first commit

---
+++ app/api/adder/route.ts
@@ -0,0 +1,14 @@
+// export default (req, res) => {
+//   const { count } = req.query;
+//   // Use the count value as needed, for example, add it to a fixed value
+//   const result = parseInt(count, 10) + 42;
+//   res.json({ result });
+// };
+
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+
+  const newCount = parseInt(searchParams.get("count")) + 1;
+
+  return Response.json({ count: newCount });
+}
