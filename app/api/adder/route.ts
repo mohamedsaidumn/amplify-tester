a41414f9903340ee47f82a995be8958ff096d80e
mohamedsaid321 <mohamedsaidumn@gmail.com>
2024-02-09 23:29:05 -0600

    corrected null and string issue

--- app/api/adder/route.ts
+++ app/api/adder/route.ts
@@ -8,7 +8,7 @@
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
 
-  const newCount = parseInt(searchParams.get("count")) + 1;
+  const newCount = parseInt(searchParams.get("count")!) + 1;
 
   return Response.json({ count: newCount });
 }
